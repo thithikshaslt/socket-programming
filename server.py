66ecdc5522bc19dc31f1ba523ce8dc3f02808d01
Thithiksha <thithiksha.slt@gmail.com>
2024-04-10 00:35:03 +0530

    update files

--- server.py
+++ server.py
@@ -1,12 +1,26 @@
 from datetime import datetime
 import socket
 
-serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #creating a socket 
+# Create a socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Get local machine name
 host = socket.gethostname()
-port = 9990
+port = 9994
 
-serversocket.bind(host,port)
+# Bind to the port
+serversocket.bind((host, port))
 
+# Queue up to 5 requests
 serversocket.listen(5)
 
+while True:
+    # Establish a connection
+    clientsocket, addr = serversocket.accept()
+    print(f"Got a connection from {str(addr)}")
+
+    # Get current time
+    currentTime = str(datetime.now())
+    # Send current time to client
+    clientsocket.send(currentTime.encode('ascii'))
+    clientsocket.close()
